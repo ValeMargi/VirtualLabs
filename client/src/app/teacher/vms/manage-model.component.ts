2b64c0bc85eae8fa4f8ab8f8783ceaaee9c5b187
Alexandro <alexandervax@gmail.com>
2020-08-18 00:48:56 +0200

    Controlli gestione modello VM (teacher)

--- client/src/app/teacher/vms/manage-model.component.ts
+++ client/src/app/teacher/vms/manage-model.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Course } from 'src/app/models/course.model';
 import { TeacherService } from 'src/app/services/teacher.service';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-model',
@@ -10,8 +11,19 @@ import { TeacherService } from 'src/app/services/teacher.service';
 export class ManageModelComponent implements OnInit {
 
   @Input() modelvm: Course;
+  ModelVmForm: FormGroup;
 
-  constructor(private teacherService: TeacherService) { }
+  constructor(private teacherService: TeacherService,
+    private formBuilder: FormBuilder) { 
+
+      this.ModelVmForm = this.formBuilder.group({
+        max_vcpu : new FormControl('', [Validators.required, Validators.min(1),Validators.max(24)]),
+        max_disco : new FormControl('', [Validators.required, Validators.min(10),Validators.max(500)]),
+        max_ram : new FormControl('', [Validators.required, Validators.min(1),Validators.max(250)]),
+        max_vm : new FormControl('', [Validators.required, Validators.min(1),Validators.max(10)]),
+        max_vm_active : new FormControl('', [Validators.required, Validators.min(1),Validators.max(10)])
+      });
+    }
 
   ngOnInit(): void {
    

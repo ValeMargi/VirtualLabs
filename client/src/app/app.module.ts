102591d67c6fa4e02ca774b68efea21cedbf3651
Giacomo Gorga <gorga@feedbackitalia.it>
2020-10-08 22:21:52 +0200

    Add module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -64,6 +64,8 @@ import { AddCourseContComponent } from './teacher/add-course/add-course-cont.com
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { FooterComponent } from './footer/footer.component';
 import { UserNotAllowedComponent } from './not-allowed/user-not-allowed.component';
+import { HomeworksContComponent as HomeworksContComponentStudent } from './student/assignments/homeworks/homeworks-cont/homeworks-cont.component';
+import { HomeworksComponent as HomeworksComponentStudent } from './student/assignments/homeworks/homeworks.component';
 import { VersionsComponent as VersionsComponentStudent } from './student/assignments/versions/versions.component';
 import { VersionsContComponent as VersionsContComponentStudent } from './student/assignments/versions/versions-cont/versions-cont.component';
 import { RegisterSuccessComponent } from './register-success/register-success.component';
@@ -113,6 +115,8 @@ import { ManageVmContComponent } from './student/vms/manage-vm/manage-vm-cont/ma
     ForgotPasswordComponent,
     FooterComponent,
     UserNotAllowedComponent,
+    HomeworksComponentStudent,
+    HomeworksContComponentStudent,
     VersionsComponentStudent,
     VersionsContComponentStudent,
     RegisterSuccessComponent,

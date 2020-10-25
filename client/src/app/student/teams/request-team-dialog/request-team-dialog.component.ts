aee9803c6034bb825ada8362abc6bb7d3c2b5e34
Giacomo Gorga <gorga@feedbackitalia.it>
2020-10-25 19:12:09 +0100

    Validazione FormGroup create team

--- client/src/app/student/teams/request-team-dialog/request-team-dialog.component.ts
+++ client/src/app/student/teams/request-team-dialog/request-team-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import * as moment from 'moment';
 @Component({
   selector: 'app-request-team-dialog',
@@ -13,38 +13,36 @@ export class RequestTeamDialogComponent implements OnInit{
     {value: "14", viewValue: "2 settimane"}
   ];
 
-  defaultTimeout= this.timeouts[0].value;
+  defaultTimeout = this.timeouts[0].value;
+  CreateTeamForm: FormGroup;
 
   @Output('propose') propose = new EventEmitter<any>();
 
-  form = {
-    name : new FormControl('', [Validators.required, Validators.minLength(3)]),
-    date : new FormControl('', [Validators.required]),
-    members: new FormControl('', [Validators.required])
+  constructor(private formBuilder: FormBuilder) {
+    this.CreateTeamForm = formBuilder.group({
+      name : new FormControl('', [Validators.required, Validators.minLength(3)]),
+      date : new FormControl('', [Validators.required]),
+      members: new FormControl('', [Validators.required])
+    });
   }
 
-  constructor() {}
   ngOnInit() {}
 
-  getErrorMessage() {
-    if (this.form.name.hasError('required') || this.form.date.hasError('required')) {
-      return 'Campo obbligatorio';
-    }
-    if(this.form.name.hasError('minlength')){
-      return 'Inserire almeno 3 caratteri';
-    }
-  }
-
   proposeTeam(nameTeam: string, days: string, members: string) {
-    let timeout: string = moment(new Date().setDate(new Date().getDate() + Number.parseInt(days))).format("YYYY-MM-DD HH:mm:ss.SSS");
-    let membersToTrim: string[] = members.split(",");
-    let membersId: string[] = [];
+    if (this.CreateTeamForm.valid) {
+      let timeout: string = moment(new Date().setDate(new Date().getDate() + Number.parseInt(days))).format("YYYY-MM-DD HH:mm:ss.SSS");
+      let membersToTrim: string[] = members.split(",");
+      let membersId: string[] = [];
 
-    membersToTrim.forEach(m => {
-      membersId.push(m.trim());
-    });
-    
-    this.propose.emit({teamName: nameTeam, timeout: timeout, membersId: membersId});
+      membersToTrim.forEach(m => {
+        membersId.push(m.trim());
+      });
+
+      this.propose.emit({teamName: nameTeam, timeout: timeout, membersId: membersId});
+    }
+    else {
+      window.alert("Controllare di aver inserito dei campi validi e riprovare");
+    }
   }
 
 }

9bf4945074dbdde2e79a2db638f0683aafcb149c
Valentina <s267543@studenti.polito.it>
2020-10-27 14:28:53 +0100

    Fix expired time token to 1h

--- server/src/main/java/it/polito/ai/virtualLabs/security/JwtTokenUtil.java
+++ server/src/main/java/it/polito/ai/virtualLabs/security/JwtTokenUtil.java
@@ -23,7 +23,7 @@ import org.springframework.web.server.ResponseStatusException;
 @Component
 public class JwtTokenUtil implements Serializable {
     private static final long serialVersionUID = -2550185165626007488L;
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+    public static final long JWT_TOKEN_VALIDITY = 60 * 60;
     @Value("${jwt.secret}")
     private String secret;
     static final String CLAIM_KEY_ROLE="role";

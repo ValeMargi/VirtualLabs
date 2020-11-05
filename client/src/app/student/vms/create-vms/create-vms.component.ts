66df4e207e59f885dd8e2b3e2eb926cdf9e21ecf
Giacomo Gorga <gorga@feedbackitalia.it>
2020-11-06 00:34:01 +0100

    Compila (da terminare)

--- client/src/app/student/vms/create-vms/create-vms.component.ts
+++ client/src/app/student/vms/create-vms/create-vms.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, AfterViewChecked, AfterViewInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, AfterViewInit, Output, EventEmitter, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
 import { VM } from 'src/app/models/vm.model';
 import { Team } from 'src/app/models/team.model';
 @Component({
@@ -8,37 +8,61 @@ import { Team } from 'src/app/models/team.model';
   templateUrl: './create-vms.component.html',
   styleUrls: ['./create-vms.component.css']
 })
-export class CreateVmsComponent implements OnInit {
-
-  @Output('create') create = new EventEmitter<VM>();
+export class CreateVmsComponent implements OnInit, OnChanges {
 
   @Input() team: Team;
+  @Output('create') create = new EventEmitter<VM>();
 
   createVmForm: FormGroup;
 
   constructor(private matDialogRef: MatDialogRef<CreateVmsComponent>,
               private formBuilder: FormBuilder) {
 
-    let maxVcpu = this.team.maxVpcuLeft;
-    console.log(maxVcpu);
-
     this.createVmForm =this.formBuilder.group({
       name: new FormControl('', [Validators.required]),
-      vcpu: new FormControl('', [Validators.required, Validators.max(maxVcpu)]),
-      ram: new FormControl('', [Validators.required]),
-      disk: new FormControl('', [Validators.required])
-    },{ validator: this.checkResources});
+      vcpu: new FormControl('', [Validators.required, Validators.min(1)]),
+      ram: new FormControl('', [Validators.required, Validators.min(1)]),
+      disk: new FormControl('', [Validators.required, Validators.min(1)])
+    });
    }
 
   ngOnInit(): void {
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.team != null) {
+      this.team = changes.team.currentValue;
+
+      /*if (this.team != null) {
+        this.createVmForm =this.formBuilder.group({
+          name: new FormControl('', [Validators.required]),
+          vcpu: new FormControl('', [Validators.required, Validators.min(1), Validators.max(this.team.maxVpcuLeft)]),
+          ram: new FormControl('', [Validators.required, Validators.min(1)]),
+          disk: new FormControl('', [Validators.required, Validators.min(1)])
+        });
+      }*/
+    }
   }
 
   close() {
     this.matDialogRef.close();
   }
 
-  checkResources(group: FormGroup){
+  updateForm() {
+    return (control: AbstractControl) => {
+      return this.checkResources(control.value)/*.pipe(
+        map(res => {
+          // if res is true, username exists, return true
+          return res ? { usernameExists: true } : null;
+          // NB: Return null if there is no error
+        })
+      );*/
+    };
+  }
 
+  checkResources(value) {
+    console.log(value)
   }
 
   createVM(vcpu: number, diskSpace: number, ram: number, name: string) {

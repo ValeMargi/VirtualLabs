2b2394c72145eb6c4654d1666120e8a446cd1fc9
Giacomo Gorga <gorga@feedbackitalia.it>
2020-11-15 17:16:09 +0100

    Fix checkbox default add course

--- client/src/app/teacher/add-course/add-course-dialog.component.ts
+++ client/src/app/teacher/add-course/add-course-dialog.component.ts
@@ -12,6 +12,7 @@ import { FormControl, Validators, FormGroup, FormBuilder, FormGroupDirective, Ng
 import { ManageModelContComponent } from '../vms/manage-model-cont.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-add-course-dialog',
@@ -20,21 +21,22 @@ import { ErrorStateMatcher } from '@angular/material/core';
 })
 export class AddCourseDialogComponent implements OnInit, OnChanges {
   @ViewChild('table') table: MatTable<Element>;
+  @ViewChild('fileInput') fileInput: MatInput;
 
   private sort: MatSort;
   panelOpenState: boolean;
   ModelVmForm: FormGroup;
-  valueDefault: boolean = false;
+  valueDefault: boolean = true;
 
-  courseName: string;
-  acronym: string;
-  maxMembers: number;
-  minMembers: number;
-  maxVcpu: number;
-  maxDisk: number;
-  ram: number;
-  totInstances: number;
-  runningInstances: number;
+  courseName: string = "";
+  acronym: string = "";
+  maxMembers: number = 4;
+  minMembers: number = 1;
+  maxVcpu: number = 4;
+  maxDisk: number = 100;
+  ram: number = 8;
+  totInstances: number = 10;
+  runningInstances: number = 10;
 
 
   @ViewChild(MatSort) set matSort(ms: MatSort) {
@@ -64,24 +66,21 @@ export class AddCourseDialogComponent implements OnInit, OnChanges {
   @Output('add') add = new EventEmitter<any>();
 
   constructor(private cont: AddCourseContComponent,
-              private formBuilder: FormBuilder,
-              private router: Router,
-              private route: ActivatedRoute) {
+              private formBuilder: FormBuilder) {
 
       this.AddCourseForm = this.formBuilder.group({
         name : new FormControl('', [Validators.required]),
         acronym : new FormControl('', [Validators.required]),
-        max_iscrizioni : new FormControl({value: '4'}, [Validators.required, Validators.min(1)]),
-        min_iscrizioni : new FormControl({value: '1'}, [Validators.required, Validators.min(1)]),
+        max_iscrizioni : new FormControl('', [Validators.required, Validators.min(1)]),
+        min_iscrizioni : new FormControl('', [Validators.required, Validators.min(1)]),
       },{ validator: Validators.compose([CustomValidators.maxMemberValidator, CustomValidators.minMemberValidator])});
 
       this.ModelVmForm = this.formBuilder.group({
-        max_vcpu : new FormControl({value:'1', disabled: false}, [Validators.required, Validators.min(1)]),
+        max_vcpu : new FormControl('', [Validators.required, Validators.min(1)]),
         max_disco : new FormControl('', [Validators.required, Validators.min(1)]),
         max_ram : new FormControl('', [Validators.required, Validators.min(1)]),
         max_vm : new FormControl('', [Validators.required, Validators.min(1)]),
-        max_vm_active : new FormControl('', [Validators.required, Validators.min(1)]),
-        imageVM: new FormControl('', [Validators.required])
+        max_vm_active : new FormControl('', [Validators.required, Validators.min(1)])
       }, {validator: this.maxVmValidator});
 
      }
@@ -138,11 +137,6 @@ export class AddCourseDialogComponent implements OnInit, OnChanges {
     this.teacherSelected = teacher;
   }
 
-
-  deselectDefault(){
-    //console.log(event);
-    this.setDefaultValue(false);
-  }
   maxVmValidator(group: FormGroup) {
     let max_vm: number = group.controls.max_vm.value;
     let max_vm_active: number = group.controls.max_vm_active.value;
@@ -166,18 +160,34 @@ export class AddCourseDialogComponent implements OnInit, OnChanges {
     }
   }
 
-  setDefaultValue( defaultCheck: boolean){
+  isDefault() {
+    if (this.valueDefault) {
+      this.ModelVmForm.controls['max_vcpu'].disable();
+      this.ModelVmForm.controls['max_disco'].disable();
+      this.ModelVmForm.controls['max_ram'].disable();
+      this.ModelVmForm.controls['max_vm'].disable();
+      this.ModelVmForm.controls['max_vm_active'].disable();
+    }
+    else {
+      this.ModelVmForm.controls['max_vcpu'].enable();
+      this.ModelVmForm.controls['max_disco'].enable();
+      this.ModelVmForm.controls['max_ram'].enable();
+      this.ModelVmForm.controls['max_vm'].enable();
+      this.ModelVmForm.controls['max_vm_active'].enable();
+    }
+
+    return this.valueDefault;
+  }
+
+  setDefaultValue(defaultCheck: boolean){
     this.valueDefault = defaultCheck;
 
-    if(defaultCheck){
-      this.ModelVmForm.setValue({
-        max_vcpu: 1,
-        max_disco: 100,
-        max_ram: 4,
-        max_vm: 10,
-        max_vm_active: 8,
-        imageVM: ""
-      });
+    if (defaultCheck) {
+      this.maxVcpu = 4;
+      this.maxDisk = 100;
+      this.ram = 8;
+      this.totInstances = 10;
+      this.runningInstances = 10;
     }
   }
 
@@ -204,16 +214,10 @@ export class AddCourseDialogComponent implements OnInit, OnChanges {
       window.alert("Controllare che i dati inseriti rispettino tutti i vincoli");
       return;
     }
-
-    console.log(name);
-    console.log(acronym);
-    console.log(min);
-    console.log(max);
-    console.log(maxCpu);
-    console.log(maxDisk);
-    console.log(ram);
-    console.log(totInstances);
-    console.log(runnigInstance);
+    else if (this.selectedPhoto == null) {
+      window.alert("Inserire un'immagine per la VM");
+      return;
+    }
 
     let course = new Course(name.toLowerCase().split(' ').join('-'), acronym.toUpperCase(), min, max, 1, maxCpu, maxDisk, ram, runnigInstance, totInstances );
 
